3e0cde6347807034161d89156a372f54b4e333f9
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-11-07 11:45:38 +0100

    more oauth implementations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use actix_cors::Cors;
 use actix_web::web;
 use actix_web::App;
@@ -9,13 +11,26 @@ use polls::routes::save_poll;
 use oauth::routes::oauth_url;
 use oauth::routes::authenticate;
 
+pub fn panic_on_missing_env() {
+    env::var("DISCORD_CLIENT_ID").expect("Expected DISCORD_CLIENT_ID in the environment");
+    env::var("DISCORD_CLIENT_SECRET").expect("Expected DISCORD_CLIENT_SECRET in the environment");
+    env::var("CLIENT_URI").expect("Expected CLIENT_URI in the environment");
+    env::var("DB_HOST").expect("Expected DB_HOST in the environment");
+    env::var("DB_NAME").expect("Expected DB_NAME in the environment");
+    env::var("DB_PASSWORD").expect("Expected DB_PASSWORD in the environment");
+    env::var("DB_USER").expect("Expected DB_USER in the environment");
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().expect("Failed to load .env file");
+    panic_on_missing_env();
+
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::default()
                 .allowed_methods(vec!["GET", "POST"])
-                .allowed_origin("http://127.0.0.1:3000")
+                .allowed_origin(&env::var("CLIENT_URI").expect("Expected CLIENT_URI in the environment"))
                 .allowed_header(http::header::CONTENT_TYPE)
             )
             .route("/api/polls/save", web::post().to(save_poll))
